d9548abc667f726e00df3cee22d50bedaac80e11
Whitney <whitney.merkel@gmail.com>
2013-06-17 21:54:09 -0400

    exercise 3 ruby the hard way

---
+++ ex3.rb
@@ -0,0 +1,59 @@
+# your counting your chickens (dhurr)
+puts "I will now count my chickens:"
+
+# P.E.M.D.A.S
+# Parenthesis
+# Exponents
+# Multiplication and Division (same ranking; do from left to right)
+# Addition and Subtraction (same ranking; do from left to right)
+
+# divide 30 by 6 and add 25 to the quotient
+puts "Hens", 25 + 30 / 6
+
+
+puts "Roosters", 100 - 25 * 3 % 4
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 <  5 - 7?"
+
+puts 3+ 2 < 5 - 7
+
+puts "What is 3 + 2?", 3 + 2
+puts "What is 5 - 7?", 5 - 7
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2
+
+# floating point number
+# divide 30 by 6 and add 25 to the quotient
+puts "Hens", 25.0 + 30.0 / 6.0
+
+
+puts "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+puts "Now I will count the eggs:"
+
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+puts "Is it true that 3.0 + 2.0 <  5.0 - 7.0?"
+
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+puts "What is 3.0 + 2.0?", 3.0 + 2.0
+puts "What is 5.0 - 7.0?", 5.0 - 7.0
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater?", 5.0 > -2.0
+puts "Is it greater or equal?", 5.0 >= -2.0
+puts "Is it less or equal?", 5.0 <= -2.0
