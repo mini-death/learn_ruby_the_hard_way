c69bec2c6db4ede90b96e45eb59b51b6733e2555
Whitney <whitney.merkel@gmail.com>
2013-06-28 09:06:03 -0400

    started ex7.rb learn ruby the hard way

---
+++ ex7.rb
@@ -0,0 +1,8 @@
+puts "Mary had a little lamb."
+puts "It's fleece was white as %s." % 'snow'
+puts "And everywhere that Mary went."
+puts "." * 10 # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
