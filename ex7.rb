a46cf205d7bc501e5a560ddebb5c4162085cf561
Whitney <whitney.merkel@gmail.com>
2013-06-28 20:52:27 -0400

    ex7.rb learn ruby the hard way

--- ex7.rb
+++ ex7.rb
@@ -3,6 +3,29 @@ puts "It's fleece was white as %s." % 'snow'
 puts "And everywhere that Mary went."
 puts "." * 10 # what'd that do?
 
+# all the "end"'s are like variables. they swap out what ever you make them stand for.
+
 end1 = "C"
 end2 = "h"
 end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+
+# when you leave it with print, cheesesburger all prints on the same line.
+# when you change it to puts, cheese prints on one line and burger prints on another one.
+
+# notice how we are using prints instead of puts here. change it to puts
+# and see what happens.
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
+
+# this just is polite use of the terminal, try removing it
+puts
