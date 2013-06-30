6070e021181065084489e495309260dd4a53e72c
Whitney <whitney.merkel@gmail.com>
2013-06-29 22:45:56 -0400

    ex9.rb learn ruby the hard way

---
+++ ex9.rb
@@ -0,0 +1,16 @@
+# Here's some new strange stuff, remember type it exactly.
+
+# the 'days' is just another variable but the \n in the months means new line.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: ", days
+puts "Here are the months: ", months
+
+puts <<PARAGRAPH
+There's something going on here.
+with the PARAGRAPH thing
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+PARAGRAPH
