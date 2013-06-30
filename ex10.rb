421bccb8364c24815d9f03238d66156f6fec035b
Whitney <whitney.merkel@gmail.com>
2013-06-29 23:53:07 -0400

    ex10.rb improved learn ruby the hard way

--- ex10.rb
+++ ex10.rb
@@ -1,3 +1,12 @@
+# \t is basically just a shorthand version of the word tab,
+# a single slash in "" means run.
+# whatever shorthand (\t; \n) you have behind the slash (\) makes it go
+# a single slash in '' means that whatever shorthand you have behind the slash
+# doesn't run it
+# but only shows the actually thing you typed
+# ('\thello' would print out the exact same way)
+
+
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
@@ -13,3 +22,5 @@ puts tabby_cat
 puts persian_cat
 puts backslash_cat
 puts fat_cat
+
+
