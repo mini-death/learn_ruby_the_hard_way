035347da4d3d1fd196b33b88f17dc1fe521dc133
Whitney <whitney.merkel@gmail.com>
2013-07-06 12:09:21 -0400

    ex17.rb and hhh.txt learn ruby the hard way

---
+++ ex17.rb
@@ -0,0 +1,24 @@
+# from_file and to_file are both arguments for argv. from is the 0 and to is the 1
+# the from file is copying all its
+from_file, to_file = ARGV
+script = $0
+
+puts "Copying from #{from_file} to #{to_file}"
+
+# we could do these on one line to, how?
+input = File.open(from_file)
+indata = input.read()
+
+puts "The input file is #{indata.length} bytes long"
+
+puts "Does the output file exist? #{File.exists? to_file}"
+puts "Ready, hit RETURN to continue, CTRL-C to abort."
+STDIN.gets
+
+output = File.open(to_file, 'w')
+output.write(indata)
+
+puts "Alright, all done."
+
+output.close()
+input.close()
