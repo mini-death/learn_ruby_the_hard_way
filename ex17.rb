f379882da9d7346956b878f5a3ec67bcccaa8d64
Whitney <whitney.merkel@gmail.com>
2013-07-06 12:15:16 -0400

    ex17.rb learn ruby the hard way

--- ex17.rb
+++ ex17.rb
@@ -1,5 +1,5 @@
 # from_file and to_file are both arguments for argv. from is the 0 and to is the 1
-# the from file is copying all its
+# the from file is copying all its  contents into the to file.
 from_file, to_file = ARGV
 script = $0
 
