7d36c0359cf0b47db9a9abd8f7a8469477ffce37
Whitney <whitney.merkel@gmail.com>
2013-07-09 15:45:29 -0400

    ex22.rb learn ruby the hard way

---
+++ ex22.rb
@@ -0,0 +1,146 @@
+=begin
+
+#  =  pound =   we use this symbol before we write a comment and in #{} variables
+
+""  =  double quotes = we use double quotes right before and after a string
+
+puts  = this word means put string, we use it when we put strings
+
+!  = exclamation mark = we use this in strings where we want to put emphasis
+
+.  = period = we use this in a string when we want to end a sentence or to abbreviate long sets of words
+
+
+''  =
+
+
+'  = apostrophe = we use these in strings when we abbreviate words like I'm or doesn't
+
+:  = colon = these are what we use before lists or between saying you'll do something and doing it.
+
+,  = comma = we use these to separate words and numbers and to take breaths during sentences
+
++  = plus = we use this in addition
+
+/  = forward slash = we use this for dividing
+
+-  = dash = we use this for subtraction or for negative numbers
+
+
+%  =
+
+
+<  = less than sign = we use this in math when we want to find out if something is less than something
+
+?  = question mark = we use this in strings when we ask questions
+
+>  = greater than sign = we use these in math when we want to find out if something is more than something
+
+floating point number  = these are just numbers with decimals
+
+*  = splat = used in math for multiplication
+
+<= = less than/equal to = used in math to see if something is less that or equal to something else
+
+>= = greater than/equal to = just like <= (look above) only greater than
+
+= = equals = used in variables and before the answer to math equations
+
+_  = underscore = used in multi worded variables
+
+{}  = curly brackets = used in #{} variables
+
+
+
+[]  = square brackets
+
+
+end  =
+
+
+
+formatter  =
+
+
+
+%s  = percent s =  this means "hold this spot, something is coming"
+
+\n  = backslash n = this means new line
+
+<< = double less than = you put this before certain words
+
+\t = backslash t = this means tab
+
+
+<<MY_HERODOC =
+
+
+
+gets.chomp()  =
+
+
+
+print  =
+
+
+
+open uri  =
+
+
+ARGV  = ARGV is an argument variable that passes arguments through the ruby script
+
+(ARGV) first, second, third  = the three arguments you give argv
+
+
+$0  =
+
+
+
+<<MESSAGE  =
+
+
+
+STDIN.gets.chomp()  =
+
+
+()  =
+
+
+target.write  =
+
+
+truncate  = it is unpacking a file
+
+
+target.close  =
+
+
+
+File.open  =
+
+
+
+output.closed/input.closed  =
+
+
+
+
+indata  =
+
+
+
+
+puts_two  = a function that accepts many arguments
+
+args  = arguments
+
+def  = define
+
+
+
+IO::SEEK_SET  =
+
+
+
+
+=end
