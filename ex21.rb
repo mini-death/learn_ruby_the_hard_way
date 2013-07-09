947bac9310a90b78a01fc9525477ca136a19b7af
Whitney <whitney.merkel@gmail.com>
2013-07-09 09:33:52 -0400

    ex21.rb learn ruby the hard way

---
+++ ex21.rb
@@ -0,0 +1,41 @@
+# a and b are variables that we give the value to
+# all the defs are explaining to ruby what to do with the numbers we type in
+# a is the first number and b is the second number
+# then it does the equation with the numbers we enter in
+# and the operation symbol between a and b
+
+def add(a, b)
+  puts "ADDING #{a} + #{b}"
+  a + b
+end
+
+def subtract(a, b)
+  puts "SUBTRACTING #{a} - #{b}"
+  a - b
+end
+
+def multiply(a, b)
+  puts "MULTIPLYING #{a} * #{b}"
+  a * b
+end
+
+def divide(a, b)
+  puts "DIVIDING #{a} / #{b}"
+  a / b
+end
+
+puts "Let's do some math with just functions!"
+
+age = add(35, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
+
+# A puzzle for the extra credit, type it in anyway.
+puts "Here is a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+puts "That becomes: #{what} Can you do it by hand?"
